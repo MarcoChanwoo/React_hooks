42c45eb8cec35129b54507e488787f5fd9210a85
MarcoChanwoo <cwhong8087@gmail.com>
2023-08-15 20:45:36 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Counter from './Counter';
+import Info from './info';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+   const [visible, setVisible] = useState(false);
+
+   return (
+      <div>
+         <button
+            onClick={() => {
+               setVisible(!visible);
+            }}
+         >
+            {visible ? '숨기기' : '보이기'}
+         </button>
+         <hr />
+         {visible && <Info />}
+      </div>
+   );
+};
 
 export default App;
